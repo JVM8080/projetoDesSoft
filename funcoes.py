df45f914465222407ff782eee7c928b85a2e04db
JVM <juvargas2006@hotmail.com>
2025-05-04 07:18:53 -0300

    Question 2. Closes #2

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,7 @@ def rolar_dados(number):
     for i in range(number):
         vector.append(randint(1,6))
     return vector
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    return [dados_rolados[:dado_para_guardar] + dados_rolados[dado_para_guardar + 1:], dados_no_estoque]
