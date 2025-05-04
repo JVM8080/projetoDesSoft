ed890f14c743a6953b5dd4e32535acfeeb68b4bf
JVM <juvargas2006@hotmail.com>
2025-05-04 07:24:58 -0300

    Question 3. Closes #3

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,7 @@ def rolar_dados(number):
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque.append(dados_rolados[dado_para_guardar])
     return [dados_rolados[:dado_para_guardar] + dados_rolados[dado_para_guardar + 1:], dados_no_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dados_rolados.append(dados_no_estoque[dado_para_remover])
+    return [dados_rolados, dados_no_estoque[:dado_para_remover] + dados_no_estoque[dado_para_remover + 1:]]
